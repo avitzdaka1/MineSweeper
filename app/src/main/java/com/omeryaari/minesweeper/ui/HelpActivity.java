7c4fa71b097b9cd2e8f867817473039af943418f
Omer Yaari <omermyaari@gmail.com>
2016-12-02 18:59:20 +0200

    02/12

---
+++ app/src/main/java/com/omeryaari/minesweeper/ui/HelpActivity.java
@@ -0,0 +1,14 @@
+package com.omeryaari.minesweeper.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.omeryaari.minesweeper.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
