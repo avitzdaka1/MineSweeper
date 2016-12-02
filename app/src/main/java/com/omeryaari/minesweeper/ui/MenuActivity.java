52c8e87652209866228a71447b0847c5e833623e
Omer Yaari <omermyaari@gmail.com>
2016-12-02 19:31:23 +0200

    friday 0212

--- app/src/main/java/com/omeryaari/minesweeper/ui/MenuActivity.java
+++ app/src/main/java/com/omeryaari/minesweeper/ui/MenuActivity.java
@@ -2,21 +2,34 @@ package com.omeryaari.minesweeper.ui;
 
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-
 import com.omeryaari.minesweeper.R;
 
 public class MenuActivity extends AppCompatActivity {
 
-    ImageView gameTitle;
+    ImageButton helpImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
         FragmentPagerAdapter adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapterViewPager);
+        helpImage = (ImageButton) findViewById(R.id.helpImage);
+
+        helpImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Intent intent = new Intent(this, )
+               // startActivity(intent);
+            }
+        });
     }
 }
